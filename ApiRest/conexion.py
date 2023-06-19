0e6f015a0c0aec04a33bfff0bb923a6fac084815
Elian Jose <116412834+ejcondorf8@users.noreply.github.com>
2023-06-19 09:50:39 -0500

    Update conexion.py

--- ApiRest/conexion.py
+++ ApiRest/conexion.py
@@ -1,5 +1,5 @@
 from peewee import *
-conexion=MySQLDatabase(database='proyecto3',user='root',password='admin',port=3306,host='localhost')
+conexion=MySQLDatabase(database='#nombreDeBase',user='root',password='admin',port=3306,host='localhost')
 
 class User(Model):
     username=CharField(max_length=10,unique=True)
